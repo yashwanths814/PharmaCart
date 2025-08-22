2ddcd1b3e69c4f58591eb61026a5f3e5bde861cb
Yashwanth S <yashwanths814@gmail.com>
2025-08-22 23:15:37 +0530

    Pharmacart Flutter Project Added

---
+++ PharmaCart/android/app/src/main/kotlin/com/example/pharmacart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pharmacart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ PharmaCart/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("PharmaCart")),
+          backgroundColor: Colors.teal, // set AppBar background color
+        ),
+        body: Center(
+          child: Text("Developed by INYA!", style: TextStyle(fontSize: 18)),
+        ),
+      ),
+    ),
+  );
+}
