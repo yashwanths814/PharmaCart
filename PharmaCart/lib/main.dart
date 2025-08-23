964620230c9c99c6e3ff1f57de9731841fc4c8ba
Yashwanth S <yashwanths814@gmail.com>
2025-08-23 22:39:17 +0530

    Add 'Get Started' button to main screen

--- PharmaCart/lib/main.dart
+++ PharmaCart/lib/main.dart
@@ -17,23 +17,16 @@ void main() {
           ),
           backgroundColor: Colors.teal, // set AppBar background color
         ),
-        body: Center(
-          child: Image.asset('images/logo.png', width: 250, height: 250),
-        ),
-        TextButton(
-          onPressed: () {},
-          child: Text(
-            'Get Started',
-            style: TextStyle(fontSize: 20),
-          ),
-          style: TextButton.styleFrom(
-            primary: Colors.white,
-            backgroundColor: Colors.teal,
-            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset('images/logo.png', width: 250, height: 250),
             ),
-          ),
+            SizedBox(height: 20), // spacing between image and text
+            Text('Get Started', style: TextStyle(fontSize: 20)),
+          ],
+        ),
       ),
     ),
   );
